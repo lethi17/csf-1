713af7bbb0603367622dfca0248a18ef59da9898
Mary Kallem <kalmar19@ccm-2610-d3.evergreen.edu>
2013-10-17 16:19:20 -0700

    Lab3

---
+++ Lab3.py
@@ -0,0 +1,23 @@
+n = 2
+
+series = 'AW MAN'
+
+x = 0
+y = 0
+z = 1
+summon = 0
+
+if series == 'fibonacci':
+   for i in range(n - 1):
+       x = y
+       y = z
+       z = x + y
+   print z
+
+elif series == 'sum':
+    for i in range(n + 1):
+        summon += (3 * i)
+    print summon
+ 
+else:
+    print "Invalid String"
