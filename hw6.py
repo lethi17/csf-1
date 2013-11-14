6d94f08cdc6f550c91543485bc240b6c4e832d0a
Mary Kallem <marykallem@gmail.com>
2013-11-14 12:39:54 -0800

    3 hours into it

--- hw6.py
+++ hw6.py
@@ -1,5 +1,5 @@
-# Name: ...
-# Evergreen Login: ...
+# Name: Mary Kallem
+# Evergreen Login: kalmar19
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 6
@@ -8,6 +8,29 @@
 # command line and copying it into the appropriate part of this file when
 # you are done.  When you are done, running this file should compute and
 # print the answers to all the problems.
+
+
+#NO PROBLEMO 3
+ages = {'Nicholas':28, 'Mary':21, 'Ulises':12}
+
+def date_of_birth(name):
+    dob = 2013 - name
+    print "She was born in " + str(dob) + "."
+
+date_of_birth(ages['Mary'])
+
+#NO PROBLEMO 4
+their_ages = {}
+#add your ages!
+
+#NO PROBLEMO 5
+
+George = 44
+Audrey = 46
+Merlin = 7
+
+fake_ages = {}
+
 
 
 ###
